57f8257cb7724fe02b937314da12b1f2573641c4
roman-h-p-spiwak <roman.h.p.spiwak@gmail.com>
2025-07-08 19:13:01 -0400

    Created `inputs.py`.
    A basic .csv reader. Due to some personalized formatting (i.e. sometimes items get placed within parenthesis, designed to fit within one column), it made more sense for a custom .csv reader.

---
+++ modules/inputs.py
@@ -0,0 +1,28 @@
+
+
+def read_csv(input: str, ) -> list:
+    data = []
+    with open(input, 'r') as file:
+        i = 0
+        for line in file:
+            data.append([])
+            entry = ""
+            control = False
+            for char in line:
+                if not control and char == ',':
+                    data[i].append(entry)
+                    entry = ""
+                    continue
+                if char == '(':
+                    entry += char
+                    control = True
+                    continue
+                if char ==')':
+                    entry += char
+                    control = False
+                    data[i].append(entry)
+                    entry = ""
+                    continue
+                entry += char
+            i += 1
+    return data
