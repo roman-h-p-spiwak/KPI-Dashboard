cc7be4e02839363ed8e3fdb76fb068eab3c48aa0
roman-h-p-spiwak <roman.h.p.spiwak@gmail.com>
2025-07-23 14:31:39 -0400

    Changed position of the graph.

--- modules/gui.py
+++ modules/gui.py
@@ -280,9 +280,9 @@ class SubGoalFrame(Page):
         
         self.content_frame = ctk.CTkFrame(self.body_frame)
         self.content_frame.grid(row=0, column=1, sticky="nsew")
-        self.content_frame.grid_rowconfigure(0, weight=1)
+        self.content_frame.grid_rowconfigure(0, weight=0)
+        self.content_frame.grid_rowconfigure(1, weight=1)
         self.content_frame.grid_columnconfigure(0, weight=1)
-        self.content_frame.grid_columnconfigure(1, weight=1)
         
         # self.sub_goal_container_frame = ctk.CTkFrame(self.content_frame)
         # # self.sub_goal_container_frame.configure(fg_color="#FF0000")
@@ -290,7 +290,7 @@ class SubGoalFrame(Page):
 
         self.sub_goal_data_frame = ctk.CTkFrame(self.content_frame)
         self.sub_goal_data_frame.grid(row=0, column=0, sticky="nsew")
-        self.sub_goal_data_frame.grid_propagate(False)
+        # self.sub_goal_data_frame.grid_propagate(False)
         self.sub_goal_data_frame.grid_rowconfigure(0, weight=0)
         self.sub_goal_data_frame.grid_rowconfigure(1, weight=0)
         self.sub_goal_data_frame.grid_rowconfigure(2, weight=0)
@@ -327,7 +327,7 @@ class SubGoalFrame(Page):
         
         self.graph_frame = ctk.CTkFrame(self.content_frame)
         # self.graph_frame.configure(fg_color="#FFFFFF")
-        self.graph_frame.grid(row=0, column=1, sticky="nsew")
+        self.graph_frame.grid(row=1, column=0, sticky="nsew")
         self.graph_frame.grid_rowconfigure(0, weight=1)
         self.graph_frame.grid_columnconfigure(0, weight=1)
         
@@ -340,12 +340,12 @@ class SubGoalFrame(Page):
     
     def show_graph(self):
         if self.show_graph_checkbox.get():
-            self.content_frame.grid_columnconfigure(0, weight=1)
-            self.content_frame.grid_columnconfigure(1, weight=1)
+            self.content_frame.grid_rowconfigure(0, weight=0)
+            self.content_frame.grid_rowconfigure(1, weight=1)
             self.graph_frame.grid()
         else:
-            self.content_frame.grid_columnconfigure(0, weight=1)
-            self.content_frame.grid_columnconfigure(1, weight=0)
+            self.content_frame.grid_rowconfigure(0, weight=1)
+            self.content_frame.grid_rowconfigure(1, weight=0)
             self.graph_frame.grid_remove()
 
     def set_graph(self, graph_path):
